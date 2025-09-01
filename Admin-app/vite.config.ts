79a4538e62503dc775c2db8a2cb250baf90518af
Emamuzo <emamuzo.okerri@gmail.com>
2025-09-01 17:30:58 +0100

    Updated vite.config.ts file: removed AutoImport and Components plugin

--- Admin-app/vite.config.ts
+++ Admin-app/vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
     visualizer({
       open: false,
       gzipSize: true,
