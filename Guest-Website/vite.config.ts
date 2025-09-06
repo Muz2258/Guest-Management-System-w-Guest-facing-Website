244e5083c03a3c321de14f6bf77ce8948eb80872
Emamuzo <emamuzo.okerri@gmail.com>
2025-09-06 01:29:34 +0100

    feat(core): Integrate unplugin-vue-components for auto-importing

--- Guest-Website/vite.config.ts
+++ Guest-Website/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import components from 'unplugin-vue-components/vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+const customResolvers = {
+  '@/utils/supabase.ts': ['supabase']
+}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +17,26 @@ export default defineConfig({
     AutoImport({
       imports: [
         'vue',
-        {
-          '@/utils/colors.ts': ['getColor', 'colors']
-        }
+        'vue-router',
+        'pinia',
+        customResolvers
       ],
+      dirs: ['./src/composables', './src/stores'],
+      dts: true
+    }),
+    components({
+      dirs: ['./src/components'],
+      deep: true,
       dts: true
     })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // This alias maps '@' to the 'src' directory
+    },
+  },
+  server: {
+    port: 5174,
+    strictPort: true,
+  }
 })
