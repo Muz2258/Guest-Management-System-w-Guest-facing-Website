88d055719c923b1ac95023b152c2a36f208f757e
Emamuzo <emamuzo.okerri@gmail.com>
2025-09-20 18:22:48 +0100

    chore(config): updated build configuration and project files
    - Updated auto-imports and component type definitions
    - Enhanced TypeScript configuration for new components
    - Updated base styles for improved component integration
    - Enhanced PaymentValidation view for consistency
    - Refined build configuration and dependencies
    - Updated project structure and type definitions

--- Guest-Website/src/views/PaymentValidation.vue
+++ Guest-Website/src/views/PaymentValidation.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="h-dvh p-32 flex flex-col justify-between bg-brand-sec-light-100">
-    <div class="logo mx-auto mb-32 flex justify-center items-center gap-x-8">
+  <main class="h-dvh p-32 flex flex-col justify-center bg-brand-sec-light-200">
+    <!-- <div class="logo mx-auto mb-32 flex justify-center items-center gap-x-8">
       <img
         class="h-8 w-auto"
         src="@/assets/vectors/leaf-branch__left.svg"
@@ -16,7 +16,7 @@
         src="@/assets/vectors/leaf-branch__right.svg"
         alt="Leaf Branch Right"
       />
-    </div>
+    </div> -->
     <div class="flex flex-col items-center justify-center">
       <picture class="mb-32">
         <source srcset="https://fonts.gstatic.com/s/e/notoemoji/latest/1f970/512.webp" type="image/webp">
@@ -27,7 +27,7 @@
       <p class="text-s text-neutrals-neu-35 text-center mb-48">We've received your gift and we are super grateful to you. Looking forward to more celebrations with you.</p>
       <Button label="Back to homepage" type="primary" @click="$router.push({ name: 'main-website' })" />
     </div>
-    <p class="text-s text-neutrals-neu-35 text-center mb-24">Copyrights 2025 <br> Cheema '25 #BuiltToLast</p>
+    <!-- <p class="text-s text-neutrals-neu-35 text-center mb-24">Copyrights 2025 <br> Cheema '25 #BuiltToLast</p> -->
   </main>
 </template>
 
