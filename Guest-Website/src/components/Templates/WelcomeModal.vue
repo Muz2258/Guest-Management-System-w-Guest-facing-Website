b66649aadefcdabc0ea3a93610e31aeb1ac92c59
Emamuzo <emamuzo.okerri@gmail.com>
2025-10-04 22:57:17 +0100

    chore: fixed display name issue in welcome modal

--- Guest-Website/src/components/Templates/WelcomeModal.vue
+++ Guest-Website/src/components/Templates/WelcomeModal.vue
@@ -65,7 +65,7 @@ const guestDisplayName = computed(() => {
   if (isCouple && lastName) {
     return `Mr. & Mrs. ${lastName}`
   }else {
-    return `${title} ${firstName}`
+    return `${title ? title : ''} ${firstName}`
   }
 })
 
